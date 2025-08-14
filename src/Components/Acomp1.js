0b61a70823ef72e1d8efa96a6d54500af148e87d
Rupali Sapkal <rupalia1624@gmail.com>
2025-08-14 22:47:42 +0530

    Added instruction file

---
+++ src/Components/Acomp1.js
@@ -0,0 +1,21 @@
+// 1.Import area
+
+import React from 'react';
+
+
+//2. Function defination area
+let Acomp1=(props) =>{
+    let name='RUPALI';
+    return (
+        <div>
+        <h1>Hello {name} {props.surname} How are you?</h1>
+       
+        </div>
+    );
+    }
+
+
+
+
+//3. Export area
+export default Acomp1;
---
+++ src/Components/Bcomp2.js
@@ -0,0 +1,13 @@
+// 1. import area
+import React from 'react';
+import {Component} from "react";
+
+//2. class definition area
+
+export class Bcomp2 extends Component {
+    fullname="Rupali Sapkal";
+    render() {
+        let n="Amit"
+        return <h1>Welcome to Home {this.fullname}</h1>;
+        }
+    }
